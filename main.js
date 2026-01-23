7ac109e8554373f316bec8446075553c9ccba1b3
loki2360-bit <loki2360@gmail.com>
2026-01-23 21:29:47 +0300

    Update main.js

--- main.js
+++ main.js
@@ -4,9 +4,6 @@ const SUPABASE_ANON_KEY = 'sb_publishable_41ROEqZ74QbA4B6_JASt4w_DeRDGXWR';
 
 // Создаём клиент Supabase
 const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-// Создаём клиент Supabase — только один раз!
-const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-
 // === Участки ===
 const stations = [
   "Распил", "ЧПУ", "Покраска", "Фрезеровка",
